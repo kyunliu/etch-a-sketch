595243b35e7200e365f96b652015508271a61eb6
Kenneth <kyunliu@outlook.com>
2023-05-11 12:43:39 -0700

    Add resetting board function

--- script.js
+++ script.js
@@ -4,6 +4,21 @@ function hoverChange(e){
     e.target.classList.add('hovered')
 }
 
+function resetBoard(){
+    const grids = container.querySelectorAll('.grid');
+    grids.forEach(grid => grid.classList.remove('hovered'));
+}
+
+const clearButton = document.getElementById('clear');
+
+clearButton.addEventListener("click", () => {
+    const userConfirmation = prompt('Are you sure you want to clear this board? Type "64" to clear.')
+    if (userConfirmation === "64"){
+        resetBoard();
+    } 
+});
+
+
 for(let i=0; i < 16; i++) {
     const row = document.createElement('div');
     row.classList.add('row');
