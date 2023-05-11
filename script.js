d19b299657f216e873034ab32ec9b8dfa404cb1d
Kenneth <kyunliu@outlook.com>
2023-05-11 11:46:46 -0700

    Change color of div when hovering

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const container = document.getElementById('container');
 
+function hoverChange(e){
+    e.target.classList.add('hovered')
+}
+
 for(let i=0; i < 16; i++) {
     const row = document.createElement('div');
     row.classList.add('row');
@@ -7,6 +11,7 @@ for(let i=0; i < 16; i++) {
     for (let j=0; j < 16; j++) {
         const grid = document.createElement('div');
         grid.classList.add('grid');
+        grid.addEventListener('mouseover', hoverChange);
     
         row.appendChild(grid);
     }
