8d0e9e334537cfbf40c98ffaeca627c041abe1bd
Kenneth <kyunliu@outlook.com>
2023-05-11 11:25:31 -0700

    Set up for loop to create 16x16 grid

---
+++ script.js
@@ -0,0 +1,15 @@
+const container = document.getElementById('container');
+
+for(let i=0; i < 16; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+
+    for (let j=0; j < 16; j++) {
+        const grid = document.createElement('div');
+        grid.classList.add('grid');
+    
+        row.appendChild(grid);
+    }
+
+    container.appendChild(row);
+}
